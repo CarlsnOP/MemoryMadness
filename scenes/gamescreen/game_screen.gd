fc679699fefd7bb9012b3bf6a8e280d034fc543a
CarlsnOP <Anderscarlsen94@gmail.com>
2024-06-09 15:15:02 +0300

    Added content till course step #99

---
+++ scenes/gamescreen/game_screen.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var sound = $Sound
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SignalManager.on_level_selected.connect(on_level_selected)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func on_level_selected(level_num: int) -> void:
+	var level_selection = GameManager.get_level_selection(level_num)
+	var frame_image = ImageManager.get_random_frame_image()
+	
+	pass
+
+func _on_exit_button_pressed():
+	SoundManager.play_button_click(sound)
+	SignalManager.on_game_exit_pressed.emit()
